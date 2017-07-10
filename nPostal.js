c2cc3b424c2a6e7ae5b918f072c5c080d1118f88
User13 <User13>
2017-07-10 15:39:59 +0300

    AMD support; test.html; gitignore;

--- nPostal.js
+++ nPostal.js
@@ -1,5 +1,18 @@
 'use strict';
-define(function(){
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(true);
+    } else {
+    	var postal = factory();
+    	window.postal = postal;
+    }
+}(this, function(){
 
 	var nPostal = function(){
 		this.events = {
@@ -88,4 +101,4 @@ define(function(){
 
 	return new nPostal();
 
-});
+}));
