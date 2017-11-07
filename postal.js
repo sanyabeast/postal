da1e093869fbb7dd2ed27f92ab1d4300225d5736
sanyabeast <a.gvrnsk@gmail.com>
2017-11-07 10:37:19 +0200

    strings concat optimize

--- postal.js
+++ postal.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
+    if (typeof define === "function" && define.amd) {
         // AMD. Register as an anonymous module.
         define(factory);
-    } else if (typeof module === 'object' && module.exports) {
+    } else if (typeof module === "object" && module.exports) {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
@@ -22,7 +22,7 @@
 
 	postal.prototype = {
 		_getEventName : function(channel, topic){
-			return channel + '::' + topic;
+			return [channel, "::", topic].join("");
 		},
 		listen : function(theme, cb, context){
 			this.ckeckEvent(theme);
